f21058f23262b6b1d9bdaf477d1385311a1aa775
nkt_dcn <indahazzard@gmail.com>
2019-05-12 15:57:58 +0300

    implemented inertia.js

---
+++ resources/js/components/Pages/Index.vue
@@ -0,0 +1,19 @@
+<template>
+    <layout>
+        <h1>Welcome</h1>
+        <p>Welcome to my first Inertia.js app!</p>
+    </layout>
+</template>
+
+<script>
+    import Layout from '@/Shared/Layout'
+
+    export default {
+        components: {
+            Layout,
+        },
+        props:{
+            'event':null
+        }
+    }
+</script>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
-
+const path = require('path');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,5 +11,19 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+    .js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .webpackConfig({
+        output: {chunkFilename: 'js/[name].[contenthash].js'},
+        resolve: {
+            alias: {
+                vue$: 'vue/dist/vue.runtime.js',
+                '@': path.resolve(
+                    __dirname,
+                    'resources/js/components'
+                ),
+            },
+            plugins: []
+        },
+    })
