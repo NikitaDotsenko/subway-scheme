b087d83e6bc6222e05d72b86f3edf2d175100062
nkt_dcn <indahazzard@gmail.com>
2019-05-13 23:06:45 +0300

    implement line canvas

--- resources/js/components/Pages/Index.vue
+++ resources/js/components/Pages/Index.vue
@@ -2,14 +2,17 @@
     <layout>
         <h1>Welcome</h1>
         <p>Welcome to my first Inertia.js app!</p>
+        <Canvas></Canvas>
     </layout>
 </template>
 
 <script>
     import Layout from '@/Shared/Layout'
+    import Canvas from "./Canvas";
 
     export default {
         components: {
+            Canvas,
             Layout,
         },
         props:{
